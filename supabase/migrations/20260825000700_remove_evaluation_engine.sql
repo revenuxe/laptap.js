e58e97c19c0c5f37c73f77a7d7593a5a8404472c
revenuxe <revenuxe@gmail.com>
2026-08-25 14:07:42 +0530

    Simplify sell flow to catalog and booking

---
+++ supabase/migrations/20260825000700_remove_evaluation_engine.sql
@@ -0,0 +1,5 @@
+DROP FUNCTION IF EXISTS public.calculate_evaluation_quote(UUID, JSONB);
+DROP TABLE IF EXISTS public.evaluation_questions CASCADE;
+DROP TABLE IF EXISTS public.model_evaluation_overrides CASCADE;
+DROP TABLE IF EXISTS public.evaluation_programs CASCADE;
+DROP TABLE IF EXISTS public.evaluation_settings CASCADE;
